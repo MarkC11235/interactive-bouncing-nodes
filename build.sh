d1ae61b6f5300b4c48f51ec07f6dde9dcaab6f20
Mark Desktop <marklouiscastle@gmail.com>
2024-03-02 14:35:50 -0600

    updated build script to run npm install

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 echo "Please enter the path that you want to build the project in: "
 read path
 echo "Building the project in $path"
+npm install
 npx electron-packager . interactive-bouncing-nodes --platform=win32 --arch=x64 --out=$path --overwrite
 echo "Project built in $path"
